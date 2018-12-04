ad71896ae29f6f75e1360976fcd1b24c9431aa7d
teoten <teoten@gmail.com>
2018-12-04 21:40:45 +0100

    elements.RData was added for info with periodic table of elements

---
+++ R/elements-data.R
@@ -0,0 +1,18 @@
+#' Elements table for EDS analysis
+#'
+#' Data frame containing information of the periodic table of elements.
+#' The data frame is based on the package PeriodicTable, data(periodicTable). The only addition to this data frame is the last two columns (EDS and EDS2) containing the energy (KeV) table for EDS analysis
+#'
+#' @docType data
+#'
+#' @usage data(elements)
+#'
+#' @format A data.frame containing info from the periodic table, obtained from package PeriodicTable
+#'
+#' @keywords elements
+#'
+#' @source \href{https://cran.r-project.org/web/packages/PeriodicTable/PeriodicTable.pdf}{PeriodicTable}
+#'
+#' @examples
+#' data(elements)
+"elements"
