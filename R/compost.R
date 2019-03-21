c6b07805360599d1dc941e4ea0238321fa748537
teoten <teoten@gmail.com>
2019-03-21 23:39:16 +0100

    Data 'compost' added for examples

---
+++ R/compost.R
@@ -0,0 +1,16 @@
+#' Results from an experiment with application of compost
+#'
+#' Results from an experiment with application of compost. The data set is suitable to perform 1- and 2-way anova.
+#' The data frame presents the directly available concentrations of Cu, Fe, K, Pb and Zn in contaminated soils after growing period of two plant species, under the addition of compost in a multifactorial set.
+#'
+#' @docType data
+#'
+#' @usage data(compost)
+#'
+#' @format Object of class data.frame, with 40 rows and 7 columns, containing 2 independent and 5 dependent variables.
+#'
+#' @keywords data, anova
+#'
+#' @source 
+#'
+#' @examples
