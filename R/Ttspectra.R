0d1dcb91e6cdea6751fa9ecdf46ccced22021028
teoten <teoten@gmail.com>
2019-06-29 13:53:42 +0200

    Tt.spectra rewritten to make ggplot2 graphics

--- R/Ttspectra.R
+++ R/Ttspectra.R
@@ -5,32 +5,75 @@
 #' @param energy vector containing the values for the x axe. Typically in KeV for EDS analysis
 #' @param counts vector containing the values for the y axe
 #' @param elements.list a vector containing the simbol of the elements to be displayed in the graphic
+#' @param lab.size size of the labels. Note that this size is considerably different for ggplot2 and basic R
 #' @param y.lim vector containing limits for y axe
 #' @param x.lim vector containing limits for x axe
+#' @param as.ggplot creates the plot using 'ggplot2' and 'scales' packages. If false, it will use base R packages
 #'
 #' @return spectra plot
 #' @keywords spectra EDS
 #' @export
 #' @examples
-#' Tt.spectra(spectrum$Energy, spectrum$Counts,elements.list=c('Pb','Zn','P'), y.lim=c(0,4000), x.lim=c(0,11)) # Simple plot with personilised limits
-#' Tt.spectra(spectrum$Energy, spectrum$Counts,elements.list=c('Ca')) # When there is overlap, as Ca and Sb, is best to add second element manually
+#' Tt.spectra(spectrum$Energy, spectrum$Counts,elements.list=c('Pb','Zn','P'), y.lim=c(0,4000), x.lim=c(0,11), as.ggplot=F) # Simple plot with personilised limits
+#' Tt.spectra(spectrum$Energy, spectrum$Counts,elements.list=c('Ca'),as.ggplot=F) # When there is overlap, as Ca and Sb, is best to add second element manually
 #' text(3.5, 500, 'Prueba', cex = 0.8)
-Tt.spectra <- function(energy, counts, elements.list, y.lim=c(0,max(counts, na.rm=T)),x.lim=c(0,max(energy, na.rm=T)))
+#' Tt.spectra(spectrum$Energy, spectrum$Counts,elements.list=c('Ca')) # However this has been fixed using 'ggplot2'
+Tt.spectra2 <- function(energy, counts, elements.list=NULL, lab.size=1,
+                       y.lim=c(0,max(counts, na.rm=T)),
+                       x.lim=c(0,max(energy, na.rm=T)),
+                       as.ggplot=TRUE)
 {
-    data(elements)    
-    plot(energy,counts,type='l', ylim=y.lim,xlim=x.lim, xlab='Energy (keV)',ylab='Counts', frame.plot=F)
+    ## as ggplot2 object
+    if (as.ggplot==T){
+        require(ggplot2)
+        require(scales)
+        xy.gg <- data.frame(energy=energy,counts=counts)
+        spectra <- ggplot(data=xy.gg, aes(x=energy, y=counts))+
+            geom_line()+
+            scale_y_continuous(limits=y.lim, oob=rescale_none)+
+            scale_x_continuous(limits=x.lim, oob=rescale_none)+
+            theme_bw()
+        spectra
+    }
+    ## Using basic R graphics
+    else{
+        plot(energy,counts,type='l',
+             ylim=y.lim,
+             xlim=x.lim,
+             xlab='Energy (keV)',
+             ylab='Counts', frame.plot=F)
+    }
+    data(elements) # loads chemical info
+    xy.elements <- data.frame()
     for (element in elements.list)
     {
-        eds <- elements[which(elements$Symbol == element),24]
+        eds <- elements[which(elements$Symbol == element),24]  # extracts eds coords
         eds2 <- elements[which(elements$Symbol == element),25]
+        ## Formatting numbers to plot
         energy <- format(round(energy, 2), nsmall = 2)
         eds <- as.numeric(format(round(eds, 2), nsmall = 2))
         eds2 <- as.numeric(format(round(eds2, 2), nsmall = 2))
         energy <- as.numeric(energy)
+        ## new local data.frames
         xy <- data.frame(energy=energy,counts=counts)
         xy.eds <- xy[xy$energy==eds,]
         xy.eds2 <- xy[xy$energy==eds2,]
-        text(eds,xy.eds$counts[1]+200,element,cex=0.8)
-        text(eds2,xy.eds2$counts[1]+200,element,cex=0.8)
+        if (as.ggplot!=T){
+            text(eds,xy.eds$counts[1]+200,element,cex=lab.size)
+            text(eds2,xy.eds2$counts[1]+200,element,cex=lab.size)
+        }
+        else {
+            xy.labs <- data.frame(x=eds,y=xy.eds$counts[1]+200,
+                                  element=element)
+            xy.elements <- rbind(xy.elements,xy.labs)
+        }
+    }
+    if (as.ggplot==T && missing(elements.list)){spectra}
+    else if (as.ggplot==T){
+        gg.labs <- geom_label(data=xy.elements,
+                              aes(x=x,y=y,label=element),
+                              size=lab.size,
+                              position=position_jitter(width=0))
+        spectra+gg.labs
     }
 }
